78791df36faed3d86855d209aa7fc359be1b6f2a
kuznetsov17 <m@viku.me>
2024-01-21 22:38:35 +0200

    fix in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 function minify_css (){
     CSS_FILES=$(find . -name *.css|xargs)
-    for FILE in ${CSS_FILES[@]}
+    for FILE in ${CSS_FILES}
         do  
             echo Minify CSS ${FILE} >> ${GITHUB_OUTPUT}
             csso -i ${FILE} -o ${FILE}
@@ -9,7 +9,7 @@ function minify_css (){
 }
 function minify_js (){
     JS_FILES=$(find . -name *.css|xargs)
-    for FILE in ${CSS_FILES[@]}
+    for FILE in ${CSS_FILES}
         do
             echo Minify JS ${FILE} >> ${GITHUB_OUTPUT}
             uglifyjs ${FILE} -m -o ${FILE}
